67db4e5401b106f690ac0762cc83f3e3cd83bd08
Darren <darhgo@gmail.com>
2026-04-28 12:31:54 -0400

    Contains Duplicate Both Optimal Sols
    Next time optimal for space, and optimal for time

--- Arrays & Hashing/Contains-Duplicate.cpp
+++ Arrays & Hashing/Contains-Duplicate.cpp
@@ -1,19 +1,29 @@
-#include <iostream>;
-#include <vector>;
-#include <unordered_map>;
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+#include <algorithm>
 
 using namespace std;
 
 class Solution
 {
 public:
-    bool containsDuplicate(vector<int>& nums) {
-        unordered_map<int, int> tracker;
+    bool containsDuplicate(vector<int> &nums)
+    {
+        /*unordered_set<int> track;
         for(int i=0; i<nums.size(); i++){
-            if(tracker.find(nums[i])!=tracker.end()){
+             if(track.find(nums[i])!=track.end()){
+                 return true;
+             }
+             track.insert(nums[i]);
+        }
+        return false;*/
+        sort(nums.begin(), nums.end());
+        for (int i = 1; i < nums.size(); i++)
+        {
+            if (nums[i] == nums[i - 1])
                 return true;
-            }
-            tracker[nums[i]]=1;
         }
         return false;
     }
@@ -23,11 +33,9 @@ int main()
 {
     Solution sol;
     vector<int> nums = {1, 2, 3, 1};
-    cout<<sol.containsDuplicate(nums)<<endl;
+    cout << sol.containsDuplicate(nums) << endl;
     vector<int> nums1 = {1, 2, 3, 4};
-    cout<<sol.containsDuplicate(nums1)<<endl;
-    vector<int> nums2 = {1,1,1,3,3,4,3,2,4,2};
-    cout<<sol.containsDuplicate(nums2)<<endl;
-
-    
+    cout << sol.containsDuplicate(nums1) << endl;
+    vector<int> nums2 = {1, 1, 1, 3, 3, 4, 3, 2, 4, 2};
+    cout << sol.containsDuplicate(nums2) << endl;
 }
