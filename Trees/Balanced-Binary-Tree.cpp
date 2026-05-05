ffc5fcd0dfefbcbb4c057706f47d816bf7be3e68
Darren <darhgo@gmail.com>
2026-05-05 11:38:25 -0400

    Balanced Binary Tree Submission 2

--- Trees/Balanced-Binary-Tree.cpp
+++ Trees/Balanced-Binary-Tree.cpp
@@ -21,29 +21,28 @@ struct TreeNode
 class Solution
 {
 public:
-    int depth(TreeNode *root)
+    int check(TreeNode *root)
     {
-        if (!root)
-        {
+        if(!root){
             return 0;
         }
-        return 1 + max(depth(root->left), depth(root->right));
+        int left=check(root->left);
+        if(left==-1) return -1;
+        int right=check(root->right);
+        if(right==-1) return -1;
+
+        if(abs(left-right)>1){
+            return -1;
+        }
+
+        return 1+max(left, right);
     }
 
     bool isBalanced(TreeNode *root)
     {
-        if (!root)
-        {
-            return true;
-        }
-        int left = depth(root->left);
-        int right = depth(root->right);
-
-        if (abs(left - right) > 1)
-        {
-            return false;
-        }
-
-        return isBalanced(root->left) && isBalanced(root->right);
+       if(check(root)==-1){
+        return false;
+       }
+       return true;
     }
 };
