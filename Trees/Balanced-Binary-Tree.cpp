6234e43a39e1971e841e91e4794e5ea8e597d37c
Darren <darhgo@gmail.com>
2026-05-05 11:15:15 -0400

    Balanced Binary Tree Submission 1

---
+++ Trees/Balanced-Binary-Tree.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+#include <algorithm>
+#include <stack>
+#include <utility>
+
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution
+{
+public:
+    int depth(TreeNode *root)
+    {
+        if (!root)
+        {
+            return 0;
+        }
+        return 1 + max(depth(root->left), depth(root->right));
+    }
+
+    bool isBalanced(TreeNode *root)
+    {
+        if (!root)
+        {
+            return true;
+        }
+        int left = depth(root->left);
+        int right = depth(root->right);
+
+        if (abs(left - right) > 1)
+        {
+            return false;
+        }
+
+        return isBalanced(root->left) && isBalanced(root->right);
+    }
+};
