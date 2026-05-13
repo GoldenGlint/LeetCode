ae90386d421377964b6b9bc96c6ba703ff5c889d
Darren <darhgo@gmail.com>
2026-05-13 12:53:34 -0400

    Two-Sum Submission #2 Optimal

--- Arrays & Hashing/Two-Sum-2.cpp
+++ Arrays & Hashing/Two-Sum-2.cpp
@@ -7,6 +7,25 @@ using namespace std;
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        
+        unordered_map <int, int> m;
+        for(int i=0; i<nums.size(); i++){
+            if(m.find(target-nums[i])!=m.end()){
+                return {m[target-nums[i]] ,i};
+            }
+            m[nums[i]]=i;
+        }
+        return {0, 0};
     }
 };
+
+int main(){ 
+    Solution sol;
+    vector<int> nums = {2, 7, 11, 15};
+    int target = 9;
+
+    Solution solution;
+    vector<int> result = solution.twoSum(nums, target);
+
+    cout << "Indices: [" << result[0] << ", " << result[1] << "]" << endl;
+    
+}
