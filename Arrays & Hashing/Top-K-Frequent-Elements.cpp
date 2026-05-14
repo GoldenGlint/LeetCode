0bb0f2f2e17c2323c4726a83cb980ccf136aef59
Darren <darhgo@gmail.com>
2026-05-14 16:05:18 -0400

    Top K Frequent Elements Submission #1

---
+++ Arrays & Hashing/Top-K-Frequent-Elements.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+class Solution
+{
+public:
+    vector<int> topKFrequent(vector<int> &nums, int k)
+    {
+        unordered_map<int, int> freq;
+        for (auto num : nums)
+        {
+            freq[num]++;
+        }
+
+        vector<pair<int, int>> myVec(freq.begin(), freq.end());
+
+        sort(myVec.begin(), myVec.end(), [](pair<int, int> a, pair<int, int> b)
+             { return a.second > b.second; });
+
+        vector<int> ans;
+
+        for (int i = 0; i < k; i++)
+        {
+            ans.push_back(myVec[i].first);
+        }
+
+        return ans;
+    }
+};
+
+int main()
+{
+    Solution sol;
+    vector<int> v1 = {1, 1, 1, 2, 2, 3};
+    vector<int> v2 = {1};
+    vector<int> v3 = {
+        1,
+        2,
+        1,
+        2,
+        1,
+        2,
+        3,
+        1,
+        3,
+    };
+    vector<int> a1 = sol.topKFrequent(v1, 2);
+    vector<int> a2 = sol.topKFrequent(v2, 1);
+    vector<int> a3 = sol.topKFrequent(v3, 2);
+
+    for (auto a : a1)
+    {
+        cout << a << " ";
+    }
+    cout << endl;
+    for (auto a : a2)
+    {
+        cout << a << " ";
+    }
+    cout << endl;
+    for (auto a : a3)
+    {
+        cout << a << " ";
+    }
+    cout << endl;
+}
