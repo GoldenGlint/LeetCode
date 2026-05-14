d5adae762b507cdca254431ff8bbfba7fae74a50
Darren <darhgo@gmail.com>
2026-05-14 15:43:31 -0400

    Group Anagram Submission #2 Optimal

--- Arrays & Hashing/Group-Anagram.cpp
+++ Arrays & Hashing/Group-Anagram.cpp
@@ -3,15 +3,29 @@
 #include <unordered_map>
 #include <unordered_set>
 #include <algorithm>
+#include <string>
 
 using namespace std;
 
 class Solution
 {
+private:
+    string getKey(string str){
+        int arr[26]={0};
+        for(char c: str){
+            arr[c-'a']++;
+        }
+        string key="";
+        for(int i=0; i<26; i++){
+            key+='#';
+            key+=to_string(arr[i]);
+        }
+        return key;
+    }
 public:
     vector<vector<string>> groupAnagrams(vector<string> &strs)
     {
-        if (strs.size() == 0)
+        if (strs.empty())
         {
             return {{}};
         }
@@ -20,11 +34,10 @@ public:
         int index = 0;
         for (int i = 0; i < strs.size(); i++)
         {
-            string temp = strs[i];
-            sort(temp.begin(), temp.end());
-            if (m.find(temp) == m.end())
+            string key=getKey(strs[i]);
+            if (m.find(key) == m.end())
             {
-                m[temp] = index;
+                m[key] = index;
                 index++;
                 vector<string> t;
                 t.push_back(strs[i]);
@@ -32,7 +45,7 @@ public:
             }
             else
             {
-                sol[m[temp]].push_back(strs[i]);
+                sol[m[key]].push_back(strs[i]);
             }
         }
         return sol;
@@ -44,7 +57,7 @@ int main()
     Solution sol;
     vector<string> str1 = {"eat", "tea", "tan", "ate", "nat", "bat"};
     vector<vector<string>> s = sol.groupAnagrams(str1);
-    for (const auto &group : s)
+    for (const auto &group : s) 
     {
         for (const auto &word : group)
         {
