62755cc5e5d26c21539204ec241e6dc5849bf894
Darren <darhgo@gmail.com>
2026-05-16 00:13:17 -0400

    Valid Sudoku Submission #2 Optimal Solution

--- Arrays & Hashing/Valid-Sudoku.cpp
+++ Arrays & Hashing/Valid-Sudoku.cpp
@@ -11,69 +11,35 @@ class Solution
 public:
     bool isValidSudoku(vector<vector<char>> &board)
     {
-        unordered_set<char> rows[9];
-        unordered_set<char> cols[9];
-        unordered_set<char> sq[9];
+        // Each bitset tracks which digits 1-9 have been seen
+        int rows[9] = {}, cols[9] = {}, boxes[9] = {};
 
-        for (int i = 0; i < board.size(); i++)
+        for (int r = 0; r < 9; r++)
         {
-            for (int l = 0; l < board[i].size(); l++)
+            for (int c = 0; c < 9; c++)
             {
-                if (board[i][l] != '.')
-                {
-                    int s;
-                    if (i < 3 && l < 3)
-                    {
-                        s = 0;
-                    }
-                    else if (i < 3 && l < 6)
-                    {
-                        s = 1;
-                    }
-                    else if (i < 3 && l < 9)
-                    {
-                        s = 2;
-                    }
-                    else if (i < 6 && l < 3)
-                    {
-                        s = 3;
-                    }
-                    else if (i < 6 && l < 6)
-                    {
-                        s = 4;
-                    }
-                    else if (i < 6 && l < 9)
-                    {
-                        s = 5;
-                    }
-                    else if (i < 9 && l < 3)
-                    {
-                        s = 6;
-                    }
-                    else if (i < 9 && l < 6)
-                    {
-                        s = 7;
-                    }
-                    else if (i < 9 && l < 9)
-                    {
-                        s = 8;
-                    }
-                    char target = board[i][l];
-                    if (rows[i].find(target) != rows[i].end() || cols[l].find(target) != cols[l].end() || sq[s].find(target) != sq[s].end())
-                    {
-                        return false;
-                    }
-                    rows[i].insert(target);
-                    cols[l].insert(target);
-                    sq[s].insert(target);
-                }
+                if (board[r][c] == '.')
+                    continue;
+
+                int val = board[r][c] - '1'; // 0-indexed digit
+                int bit = 1 << val;
+                int box = (r / 3) * 3 + (c / 3);
+
+                if (rows[r] & bit)
+                    return false;
+                if (cols[c] & bit)
+                    return false;
+                if (boxes[box] & bit)
+                    return false;
+
+                rows[r] |= bit;
+                cols[c] |= bit;
+                boxes[box] |= bit;
             }
         }
-
         return true;
     }
 };
-
 int main()
 {
     Solution sol;
