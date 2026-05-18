a0e348fc85152970c91a90bd6e41e49317ddb4fa
Darren <darhgo@gmail.com>
2026-05-18 15:47:59 -0400

    3 Sum Submission #2 Optimal

--- Two Pointers/3sum.cpp
+++ Two Pointers/3sum.cpp
@@ -10,24 +10,40 @@ class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         vector<vector<int>> sol;
+        
         sort(nums.begin(), nums.end());
-        for(int i=0; i<nums.size()-2; i++){
-            
-            int index=i;
-            if(i>0&&nums[i]==nums[i-1]){
+        int n=nums.size();
+        if (n < 3) return sol;
+        for(int i=0; i<n-2; i++){
+            if(i>0&&nums[i-1]==nums[i]){
                 continue;
             }
-            int target=-nums[i];
-            unordered_map<int, int> u;
-            for(int l=index+1; l<nums.size(); l++){
-                if(u.find(target-nums[l])!=u.end()){
-                    sol.push_back({nums[i], nums[l], target-nums[l]});
-                    while(l+1 < nums.size() && nums[l+1]==nums[l]) l++;
+            int left=i+1;
+            int right=n-1;
+            while(left<right){
+                int sum=nums[i]+nums[left]+nums[right];
+                if(sum==0){
+                    sol.push_back({nums[i], nums[left], nums[right]});
+                    while(left<right&&nums[left+1]==nums[left]){
+                    left++;
+                    }
+                    while(left<right&&nums[right-1]==nums[right]){
+                        right--;
+                    }
+                    left++;
+                    right--;
+                }   
+                else if(sum<0){
+                    left++;
                 }
-                u[nums[l]]=l;
+                else{
+                    right--;
+                }
+
 
             }
         }
+        
 
         return sol;
     }
