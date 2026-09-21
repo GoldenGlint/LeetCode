9336370a42a91648006c9c6197d756158916bb5e
GoldenGlint <darhgo@gmail.com>
2026-09-21 15:05:04 -0400

    N Queens Submission #1
    Remember the formula for diagonals

--- Backtracking/N-Queens.cpp
+++ Backtracking/N-Queens.cpp
@@ -8,11 +8,26 @@
 #include <cassert>
 using namespace std;
 
-bool checkValid(){
+void backtrack(vector<string> &board, int row, vector<bool> &col, vector<bool> &diag1, vector<bool> &diag2, int &n, vector<vector<string>> &ans, int &counter){
+    if(row>=n){
+        ans.push_back(board);
+    }
+    for(int i=0; i<n; i++){
+        if(!col[i]&&!diag1[row+i]&&!diag2[row-i+n-1]){
+            board[row][i]='Q';
+            col[i]=true;
+            diag1[row+i]=true;
+            diag2[row-i+n-1]=true;
+            counter++;
+            backtrack(board, row+1, col, diag1, diag2, n, ans, counter);
 
-}
-
-void backtrack(vector<vector<bool>> &visited, vector<string> &board, vector<bool> &col, vector<bool> &diag1, vector<bool> &diag2, int &n){
+            board[row][i]='.';
+            col[i]=false;
+            diag1[row+i]=false;
+            diag2[row-i+n-1]=false;
+            counter--;
+        }
+    }
     //row-col+n-1
     //row+col
     
@@ -27,11 +42,34 @@ public:
         vector<bool> diag1(2*n-1, false);
         vector<bool> diag2(2*n-1, false);
         int numQueens=0;
+        int row=0;
+        int counter=0;
+        vector<vector<string>> ans;
+        backtrack(board, 0, col, diag1, diag2, n, ans, counter);
+        return ans;
     }
 };
 
+void printAnswer(const vector<vector<string>>& ans) {
+    for (int i = 0; i < ans.size(); i++) {
+        cout << "Solution " << i + 1 << ":\n";
+
+        for (int j = 0; j < ans[i].size(); j++) {
+            cout << ans[i][j] << '\n';
+        }
+
+        cout << '\n';
+    }
+}
+
 int main() {
-    
+
+    Solution sol;
+
+    int n=4;
+
+    vector<vector<string>> ans=sol.solveNQueens(4);
+    printAnswer(ans);
 
     
 }
