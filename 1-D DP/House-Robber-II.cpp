763e761833d089ff2f3fb5560e27e070e387c06b
GoldenGlint <darhgo@gmail.com>
2026-09-23 19:51:02 -0400

    House Robber II Submission #1
    Optimal

---
+++ 1-D DP/House-Robber-II.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    int helper(vector<int>& nums) {
+        vector<int> tracker(nums.size(), 0);
+        if(nums.size()==1){
+            return nums[0];
+        }
+        else if(nums.size()==2){
+            return max(nums[0], nums[1]);
+        }
+        tracker[0]=nums[0];
+        tracker[1]=max(nums[0], nums[1]);
+        for(int i=2; i<tracker.size(); i++){
+            tracker[i]=max(tracker[i-1], tracker[i-2]+nums[i]);
+        }
+        return tracker[tracker.size()-1];
+    }
+    int rob(vector<int>& nums) {
+        if(nums.size()==1){
+            return nums[0];
+        }
+        vector<int>first(nums.begin()+1, nums.end());
+        vector<int>last(nums.begin(), nums.end()-1);
+        int res1=helper(first);
+        int res2=helper(last);
+        return max(res1, res2);
+    }
+};
+
+int main()
+{
+
+    Solution sol;
+    vector<int> v1={1,2,3};
+    vector<int> v2={1,2,3,1};
+    cout<<sol.rob(v1)<<endl;
+    cout<<sol.rob(v2)<<endl;
+    
+}
