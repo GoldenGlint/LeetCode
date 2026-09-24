0fe6a5fca2d79fc2604ae2da35ad6e40b5b42ec4
GoldenGlint <darhgo@gmail.com>
2026-09-23 23:33:45 -0400

    Palindromic Substrings Submission #2
    Optimal

--- 1-D DP/Palindromic-Substrings.cpp
+++ 1-D DP/Palindromic-Substrings.cpp
@@ -9,23 +9,22 @@ using namespace std;
 class Solution {
 public:
     int countSubstrings(string s) {
-        int n=s.length();
-        vector<vector<bool>> dp(n, vector<bool>(n, false));
         int counter=0;
-        for(int i=0; i<n; i++){
-            dp[i][i]=true;
-            counter++;
-        }
-
-        for(int len=2; len<=n; len++){
-            for(int i=0; i<n-len+1; i++){
-                int j=i+len-1;
-                if(s[i]==s[j]){
-                    if(len==2||dp[i+1][j-1]){
-                        counter++;
-                        dp[i][j]=true;
-                    }
-                }
+        int n=s.length();
+        for(int center=0; center<n; center++){
+            int l=center;
+            int r=center;
+            while(l>=0&&r<n&&s[l]==s[r]){
+                counter++;
+                l--;
+                r++;
+            }
+            l=center;
+            r=center+1;
+            while(l>=0&&r<n&&s[l]==s[r]){
+                counter++;
+                l--;
+                r++;
             }
         }
         return counter;
