4b398ab578700ee3b479e966774ce4a0f374dc1b
GoldenGlint <darhgo@gmail.com>
2026-09-24 17:35:08 -0400

    Maximum Product Subarray Submission #2
    Optimal, temp variable

--- 1-D DP/Maximum-Product-Subarray.cpp
+++ 1-D DP/Maximum-Product-Subarray.cpp
@@ -10,18 +10,15 @@ class Solution {
 public:
     int maxProduct(vector<int>& nums) {
         int n=nums.size();
-        vector<pair<int, int>> dp(n, {0,0});
-        dp[0]={nums[0], nums[0]};
+        int currMin=nums[0];
+        int currMax=nums[0];
+        int ans=currMax;
         for(int i=1; i<nums.size(); i++){
-            dp[i].first=max({dp[i-1].first*nums[i], dp[i-1].second*nums[i], nums[i]});
-            dp[i].second=min({dp[i-1].second*nums[i], dp[i-1].first*nums[i], nums[i]});
+            int temp=currMax;
+            currMax=max({currMax*nums[i], currMin*nums[i], nums[i]});
+            currMin=min({temp*nums[i], currMin*nums[i], nums[i]});
+            ans=max(currMax, ans);
         }
-        
-        int ans=dp[0].first;
-        for(int i=0; i<dp.size(); i++){
-            ans=max(ans, dp[i].first);
-        }
-        
         
         return ans;
 
@@ -34,7 +31,7 @@ int main()
     Solution sol;
     vector<int> v1={2,3,-2,4};
     vector<int> v2={-2,0,-1};
-    vector<int> v3={-2,3,-4};
+    vector<int> v3={-4,-3,-2};
     cout<<sol.maxProduct(v1)<<endl;
     cout<<sol.maxProduct(v2)<<endl;
     cout<<sol.maxProduct(v3)<<endl;
